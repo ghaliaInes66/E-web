e91a845c0a7e6b20cccceb4bd2b040fdeadd9da6
ines <ghalia.ines66@gmail.com>
2023-06-09 15:42:22 +0100

    update

--- public/js/mode.js
+++ public/js/mode.js
@@ -11,27 +11,26 @@ light.style.display='inline-block';
 color.style.setProperty('--color', '#0077B6');
 color.style.setProperty('--textcolor','black');
 color.style.setProperty('--backgroundColor','white');
-purple.style.display='none';
-blue.style.display='inline-block';
+
 
 
 function toLight(){
-    purple.style.display='none';
-    blue.style.display='inline-block';
     dark.style.display='none';
     light.style.display='inline-block';
     color.style.setProperty('--color', '#0077B6');
     color.style.setProperty('--textcolor','black');
     color.style.setProperty('--backgroundColor','white');
+    purple.style.display='none';
+    blue.style.display='inline-block';
 }
 function toDark(){
-    blue.style.display='none';
-    purple.style.display='inline-block';
     light.style.display='none';
     dark.style.display='inline-block';
     color.style.setProperty('--color', '#79616F');
     color.style.setProperty('--textcolor','white');
-    color.style.setProperty('--backgroundColor','rgb(46, 45, 45)')
+    color.style.setProperty('--backgroundColor','rgb(46, 45, 45)');
+    blue.style.display='none';
+    purple.style.display='inline-block';
 }
 
 btndark.addEventListener('click',toLight);
